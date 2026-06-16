1c8aa64b9c3118e7b044fa9fa1be50810203093c
Rashmika Siriwardhana <58144396+rnsrashmika078@users.noreply.github.com>
2026-06-17 00:14:13 +0530

    welcome api route

--- routes/api.php
+++ routes/api.php
@@ -5,6 +5,10 @@ use App\Http\Controllers\PostController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
+Route::get('/', function () {
+    return view('welcome');
+});
+
 Route::get('/user', function (Request $request) {
     return $request->user();
 })->middleware('auth:sanctum');
