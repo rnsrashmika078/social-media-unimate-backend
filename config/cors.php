afdf432ce05877518fc3d4e06371a5503e06904a
Rashmika Siriwardhana <58144396+rnsrashmika078@users.noreply.github.com>
2026-07-01 03:36:05 +0530

    update: cors

--- config/cors.php
+++ config/cors.php
@@ -5,7 +5,7 @@ return [
 
     'allowed_methods' => ['*'],
 
-    'allowed_origins' => ['http://20.207.201.226:3000', 'http://localhost:3000'],
+    'allowed_origins' => ['https://social-media-unimate-frontend-nine.vercel.app/sign-in', 'http://localhost:3000'],
 
 
     'allowed_origins_patterns' => [],
